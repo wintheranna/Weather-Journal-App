9e2133df13eb25286dedf97fac57dc88de36f9fd
Anna Winther <annawiinther@gmail.com>
2019-11-21 13:40:59 +0100

    Configure express to use body-parser

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ const app = express();
 /* Middleware*/
 
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
 // Cors for cross origin allowance
 
 // Initialize the main project folder
