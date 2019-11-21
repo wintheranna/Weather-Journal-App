001df52cd38a0af18de6cd2188221715ccde05e0
Anna Winther <annawiinther@gmail.com>
2019-11-21 13:48:13 +0100

    Add function to POST data

--- website/app.js
+++ website/app.js
@@ -21,6 +21,20 @@ const getData = async (baseUrl, newZip, toCelsius, apiKey) => {
 }
 
 /* Function to POST data */
+const postData = async (url='', data={}) => {
+  console.log(data);
+  const response = await fetch(url, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(data),
+  });
+  try {
+    console.log(response);
+  } catch(error) {
+      console.log('error', error);
+    }
+}
 
 
 /* Function to GET Project Data */
