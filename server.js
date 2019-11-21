74453d1344ec2a80162d34b38e3180c6dd950160
Anna Winther <annawiinther@gmail.com>
2019-11-21 13:41:46 +0100

    Add Cors

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 
